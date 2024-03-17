e090890694b4e66503ed27f5f10aa87a435cf0e8
Daniel Schrader <dhs@schrader-wolff-media.de>
2024-03-17 19:57:39 +0100

    Initial Setup
    NextJS 14
    MUI 5

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Container from '@mui/material/Container';
+import { Typography } from '@mui/material';
+
+export default function Home() {
+  return (
+    <main>
+      <Typography variant='h1'>Hello, World!</Typography>
+    </main>
+  );
+}
